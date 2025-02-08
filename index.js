07a8c9f4517f08c586fee7e487def989d6657224
Shaikat0079 <baruashaikat00790@gmail.com>
2025-02-09 08:07:50 +0930

    Array push operation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("hello World")
+//  Declare an array
+
+let new_array = ["Vagina",'Milk','Period-Blood','under-arms',"Feet"]
+console.log(new_array[3])
+new_array[2] = "Anal"
+console.log(new_array)
+
+// 3 tourist destination
+
+let destinations = ["India", "Nepal", "Japan"]
+
+destinations.push("Australia");
+
+console.log(destinations)
+
+destinations.push("Canada","England")
+
+console.log(destinations)
